ab1b71d6ece67a800bc6d9673640aaa8a645f335
BelenGigante <vinn_a@hotmail.com>
2022-12-26 13:51:43 -0500

    typed questions, corrected errors

--- index.js
+++ index.js
@@ -5,10 +5,36 @@ const fs = require('fs');
 
 inquirer.prompt([
     {
-        type:'input';
-        message: 'Whats the name of your project: ';
-        name: 'title';
-        validate: (notBlank) => if(notBlank) {return true}else {return'enter title to continue'}
+        type:'input',
+        message: 'Whats the name of your repository: ',
+        name: 'title',
+        validate: (notBlank) => {if(notBlank) {return true}else {return'enter title to continue'}}
+    },
+    {
+        type: 'input',
+        message: 'Describe your project: ',
+        name: 'description',
+        validate: (notBlank) => {if(notBlank) {return true}else {return'enter title to continue'}}
+    },
+    {
+        type: 'input',
+        message: 'Enter instalation instructions(if there are any): ',
+        name: 'instalation',
+    },
+    {
+        type: 'input',
+        message: 'Enter usage information(if there is any): ',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter contribution guidelines: ',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Enter test instructions: ',
+        name: 'tests',
     },
 
 ])
